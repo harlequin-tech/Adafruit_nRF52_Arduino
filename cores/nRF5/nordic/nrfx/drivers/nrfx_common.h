9cb91591f23747b81d251f461559e320d17f3017
Darran Hunt <darran@hunt.net.nz>
2025-12-25 20:10:29 +1300

    Add missing headers

--- cores/nRF5/nordic/nrfx/drivers/nrfx_common.h
+++ cores/nRF5/nordic/nrfx/drivers/nrfx_common.h
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2017 - 2020, Nordic Semiconductor ASA
+ * Copyright (c) 2017 - 2025, Nordic Semiconductor ASA
  * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -35,12 +37,29 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include <string.h>
+#include <limits.h>
 
-#include <nrf.h>
-#include <nrf_peripherals.h>
+// To be included before bsp/mdk to override default errata values
+#include "nrfx_errata.h"
+
+#include <nrfx_bsp.h>
+#include "drivers/nrfx_utils.h"
 
 #ifdef __cplusplus
 extern "C" {
+#endif
+
+#if defined(__CORTEX_M) || defined(__NRFX_DOXYGEN__)
+#define ISA_ARM     1
+#elif defined(__VPR_REV)
+#define ISA_RISCV   1
+#else
+#define ISA_UNKNOWN 1
+#endif
+
+#if defined(ISA_RISCV)
+#define __STATIC_INLINE __attribute__((always_inline)) static inline
 #endif
 
 #ifndef NRFX_STATIC_INLINE
@@ -50,6 +69,8 @@ extern "C" {
 #define NRFX_STATIC_INLINE __STATIC_INLINE
 #endif
 #endif // NRFX_STATIC_INLINE
+
+#define NRFY_STATIC_INLINE __STATIC_INLINE
 
 #ifndef NRF_STATIC_INLINE
 #ifdef NRF_DECLARE_ONLY
@@ -66,169 +87,17 @@ extern "C" {
  * @brief Common module.
  */
 
-/**
- * @brief Macro for checking if the specified identifier is defined and it has
- *        a non-zero value.
- *
- * Normally, preprocessors treat all undefined identifiers as having the value
- * zero. However, some tools, like static code analyzers, can issue a warning
- * when such identifier is evaluated. This macro gives the possibility to suppress
- * such warnings only in places where this macro is used for evaluation, not in
- * the whole analyzed code.
- */
-#define NRFX_CHECK(module_enabled)  (module_enabled)
+/** @brief Symbol specifying major number of the current nrfx version. */
+#define NRFX_RELEASE_VER_MAJOR 4
 
-/**
- * @brief Macro for concatenating two tokens in macro expansion.
- *
- * @note This macro is expanded in two steps so that tokens given as macros
- *       themselves are fully expanded before they are merged.
- *
- * @param[in] p1 First token.
- * @param[in] p2 Second token.
- *
- * @return The two tokens merged into one, unless they cannot together form
- *         a valid token (in such case, the preprocessor issues a warning and
- *         does not perform the concatenation).
- *
- * @sa NRFX_CONCAT_3
- */
-#define NRFX_CONCAT_2(p1, p2)       NRFX_CONCAT_2_(p1, p2)
+/** @brief Symbol specifying minor number of the current nrfx version. */
+#define NRFX_RELEASE_VER_MINOR 0
 
-/** @brief Internal macro used by @ref NRFX_CONCAT_2 to perform the expansion in two steps. */
-#define NRFX_CONCAT_2_(p1, p2)      p1 ## p2
-
-/**
- * @brief Macro for concatenating three tokens in macro expansion.
- *
- * @note This macro is expanded in two steps so that tokens given as macros
- *       themselves are fully expanded before they are merged.
- *
- * @param[in] p1 First token.
- * @param[in] p2 Second token.
- * @param[in] p3 Third token.
- *
- * @return The three tokens merged into one, unless they cannot together form
- *         a valid token (in such case, the preprocessor issues a warning and
- *         does not perform the concatenation).
- *
- * @sa NRFX_CONCAT_2
- */
-#define NRFX_CONCAT_3(p1, p2, p3)   NRFX_CONCAT_3_(p1, p2, p3)
-
-/** @brief Internal macro used by @ref NRFX_CONCAT_3 to perform the expansion in two steps. */
-#define NRFX_CONCAT_3_(p1, p2, p3)  p1 ## p2 ## p3
-
-/**
- * @brief Macro for performing rounded integer division (as opposed to
- *        truncating the result).
- *
- * @param[in] a Numerator.
- * @param[in] b Denominator.
- *
- * @return Rounded (integer) result of dividing @c a by @c b.
- */
-#define NRFX_ROUNDED_DIV(a, b)  (((a) + ((b) / 2)) / (b))
-
-/**
- * @brief Macro for performing integer division, making sure the result is rounded up.
- *
- * @details A typical use case for this macro is to compute the number of objects
- *          with size @c b required to hold @c a number of bytes.
- *
- * @param[in] a Numerator.
- * @param[in] b Denominator.
- *
- * @return Integer result of dividing @c a by @c b, rounded up.
- */
-#define NRFX_CEIL_DIV(a, b)  ((((a) - 1) / (b)) + 1)
-
-/**
- * @brief Macro for getting the number of elements in an array.
- *
- * @param[in] array Name of the array.
- *
- * @return Array element count.
- */
-#define NRFX_ARRAY_SIZE(array) (sizeof(array) / sizeof((array)[0]))
-
-/**
- * @brief Macro for getting the offset (in bytes) from the beginning of a structure
- *        of the specified type to its specified member.
- *
- * @param[in] type   Structure type.
- * @param[in] member Structure member whose offset is searched for.
- *
- * @return Member offset in bytes.
- */
-#define NRFX_OFFSETOF(type, member)  ((size_t)&(((type *)0)->member))
-
-/**@brief Macro for checking if given lengths of EasyDMA transfers do not exceed
- *        the limit of the specified peripheral.
- *
- * @param[in] peripheral Peripheral to check the lengths against.
- * @param[in] length1    First length to be checked.
- * @param[in] length2    Second length to be checked (pass 0 if not needed).
- *
- * @retval true  The length of buffers does not exceed the limit of the specified peripheral.
- * @retval false The length of buffers exceeds the limit of the specified peripheral.
- */
-#define NRFX_EASYDMA_LENGTH_VALIDATE(peripheral, length1, length2)            \
-    (((length1) < (1U << NRFX_CONCAT_2(peripheral, _EASYDMA_MAXCNT_SIZE))) && \
-     ((length2) < (1U << NRFX_CONCAT_2(peripheral, _EASYDMA_MAXCNT_SIZE))))
-
-/**
- * @brief Macro for waiting until condition is met.
- *
- * @param[in]  condition Condition to meet.
- * @param[in]  attempts  Maximum number of condition checks. Must not be 0.
- * @param[in]  delay_us  Delay between consecutive checks, in microseconds.
- * @param[out] result    Boolean variable to store the result of the wait process.
- *                       Set to true if the condition is met or false otherwise.
- */
-#define NRFX_WAIT_FOR(condition, attempts, delay_us, result) \
-do {                                                         \
-    result =  false;                                         \
-    uint32_t remaining_attempts = (attempts);                \
-    do {                                                     \
-           if (condition)                                    \
-           {                                                 \
-               result =  true;                               \
-               break;                                        \
-           }                                                 \
-           NRFX_DELAY_US(delay_us);                          \
-    } while (--remaining_attempts);                          \
-} while(0)
-
-/**
- * @brief Macro for getting the ID number of the specified peripheral.
- *
- * For peripherals in Nordic SoCs, there is a direct relationship between their
- * ID numbers and their base addresses. See the chapter "Peripheral interface"
- * (section "Peripheral ID") in the Product Specification.
- *
- * @param[in] base_addr Peripheral base address or pointer.
- *
- * @return ID number associated with the specified peripheral.
- */
-#define NRFX_PERIPHERAL_ID_GET(base_addr)  (uint8_t)((uint32_t)(base_addr) >> 12)
-
-/**
- * @brief Macro for getting the interrupt number assigned to a specific
- *        peripheral.
- *
- * For peripherals in Nordic SoCs, the IRQ number assigned to a peripheral is
- * equal to its ID number. See the chapter "Peripheral interface" (sections
- * "Peripheral ID" and "Interrupts") in the Product Specification.
- *
- * @param[in] base_addr Peripheral base address or pointer.
- *
- * @return Interrupt number associated with the specified peripheral.
- */
-#define NRFX_IRQ_NUMBER_GET(base_addr)  NRFX_PERIPHERAL_ID_GET(base_addr)
+/** @brief Symbol specifying micro number of the current nrfx version. */
+#define NRFX_RELEASE_VER_MICRO 1
 
 /** @brief IRQ handler type. */
-typedef void (* nrfx_irq_handler_t)(void);
+typedef void (* nrfx_irq_handler_t)(void *);
 
 /** @brief Driver state. */
 typedef enum
@@ -237,7 +106,6 @@ typedef enum
     NRFX_DRV_STATE_INITIALIZED,   ///< Initialized but powered off.
     NRFX_DRV_STATE_POWERED_ON,    ///< Initialized and powered on.
 } nrfx_drv_state_t;
-
 
 /**
  * @brief Function for checking if an object is placed in the Data RAM region.
@@ -269,6 +137,8 @@ NRF_STATIC_INLINE bool nrfx_is_word_aligned(void const * p_object);
 
 /**
  * @brief Function for getting the interrupt number for the specified peripheral.
+ *
+ * @warning This function is valid only for peripherals with a single interrupt line.
  *
  * @param[in] p_reg Peripheral base pointer.
  *
@@ -307,7 +177,6 @@ NRF_STATIC_INLINE uint32_t nrfx_bitpos_to_event(uint32_t bit);
  * @sa nrfx_bitpos_to_event
  */
 NRF_STATIC_INLINE uint32_t nrfx_event_to_bitpos(uint32_t event);
-
 
 #ifndef NRF_DECLARE_ONLY
 
